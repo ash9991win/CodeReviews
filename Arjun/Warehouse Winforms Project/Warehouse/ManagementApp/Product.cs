7d485ddd3d2656af87770966ff9215996491a991
Ashwin S <ashwins@ashwins-air.home>
2023-03-03 15:53:05 -0500

    Warehouse Project Code Review

---
+++ Arjun/Warehouse Winforms Project/Warehouse/ManagementApp/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementApp
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double Availability { get; set; }
+        public string Location { get; set; }
+        public string Dpci { get; set; }
+        public double Cost { get; set; }
+        public double TotalCost { get; set; }
+        public int Count { get; set; }
+        public string FullDetails { get; set; }
+
+        public List<string> Summary = new List<string>();
+
+    }
+}
---
+++ Arjun/Warehouse Winforms Project/Warehouse/ManagementApp/frmProductSummary.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ManagementApp
+{
+    partial class frmProductSummary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductCount = new System.Windows.Forms.Label();
+            this.lblTotalCost = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstPickedProducts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblProductCount
+            // 
+            this.lblProductCount.AutoSize = true;
+            this.lblProductCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductCount.Location = new System.Drawing.Point(261, 378);
+            this.lblProductCount.Name = "lblProductCount";
+            this.lblProductCount.Size = new System.Drawing.Size(0, 24);
+            this.lblProductCount.TabIndex = 9;
+            // 
+            // lblTotalCost
+            // 
+            this.lblTotalCost.AutoSize = true;
+            this.lblTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCost.Location = new System.Drawing.Point(625, 378);
+            this.lblTotalCost.Name = "lblTotalCost";
+            this.lblTotalCost.Size = new System.Drawing.Size(0, 24);
+            this.lblTotalCost.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total Items";
+            // 
+            // lstPickedProducts
+            // 
+            this.lstPickedProducts.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lstPickedProducts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstPickedProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPickedProducts.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lstPickedProducts.FormattingEnabled = true;
+            this.lstPickedProducts.ItemHeight = 16;
+            this.lstPickedProducts.Location = new System.Drawing.Point(79, 48);
+            this.lstPickedProducts.Name = "lstPickedProducts";
+            this.lstPickedProducts.Size = new System.Drawing.Size(642, 290);
+            this.lstPickedProducts.TabIndex = 5;
+            // 
+            // frmProductSummary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblProductCount);
+            this.Controls.Add(this.lblTotalCost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstPickedProducts);
+            this.Name = "frmProductSummary";
+            this.Text = "Summary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProductCount;
+        private System.Windows.Forms.Label lblTotalCost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstPickedProducts;
+    }
+}
